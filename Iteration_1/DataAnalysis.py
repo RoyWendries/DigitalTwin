91dfd0a5a14dcc63f5a2b510e1c45e5e5a6868d6
Roy Wendries <1747290wendries@zuyd.nl>
2023-03-15 13:32:42 +0100

    new main commit with dynamic file

--- Iteration_1/DataAnalysis.py
+++ Iteration_1/DataAnalysis.py
@@ -1,33 +1,36 @@
 # %%
+import os
 import json
 import pandas as pd
-import numpy as np
+
 
 # %%
 # Load Datasets & add session name
-
-
 def LoadData(count):
     name = FileList[count]
-    # uncomment and remove when not using interactive window
-    # saveLocation = "./Iteration_1/Data/" + name + ".json"
-    saveLocation = "D:/Projects/DigitalTwin/DigitalTwin/Iteration_1/Data/" + name + ".json"
-    # saveLocation = "g:/Projects/DigitalTwin/Iteration_1/Data/" + name + ".json"
-
+    saveLocation = dir + "\Data\\" + name
     with open(saveLocation) as f:
         data = json.load(f)
         data['Session'] = name
-    # ScoredSteps
     dataSS = pd.json_normalize(data, 'ScoredSteps')
     dataSS['GameSessionId'] = data['Session']
-    '''# ActionScore
-    dataAS = pd.json_normalize(data['ScoredSteps'], 'ActionScores')'''
-    return dataSS  # , dataAS
+    return dataSS
 
 
 # %%
-# Choose datasets and initiate functions
-FileList = ['test', 'Run_Daan', 'RoysUnfinishedRun']
+# Interactive windows have diff dir's
+pathExtension = "Iteration_1"
+dir = os.getcwd()
+pathCheck = dir.endswith(pathExtension)
+if not pathCheck:
+    os.chdir(pathExtension)
+    dir = os.getcwd()
+
+# %%
+# Grab datasets and initiate function to load data
+pathToJson = "Data/"
+FileList = [pos_json for pos_json in os.listdir(
+    pathToJson) if pos_json.endswith('.json')]
 
 count = len(FileList)
 while count > 0:
@@ -41,25 +44,3 @@ del olddata
 
 # ActionScore
 dataAS = newdata['ActionScores']
-
-
-# %%
-'''
-# ActionScores
-ASRandy = pd.json_normalize(DataRandy['ScoredSteps'], 'ActionScores')
-ASDaan = pd.json_normalize(DataDaan['ScoredSteps'], 'ActionScores')
-ASRoy = pd.json_normalize(DataRoy['ScoredSteps'], 'ActionScores')
-
-# Experience
-
-ExpRandy = pd.json_normalize(DataRandy, 'Experience')
-ExpDaan = pd.json_normalize(DataDaan, 'Experience')
-ExpRoy = pd.json_normalize(DataRoy, 'Experience')
-
-# Testing
-# Merged_SS.to_parquet('C:/Users/randy/Downloads/Merged_SS.parquet')
-print(Merged_SS['GameSessionId'].nunique())
-print(Merged_SS['StepId'].value_counts())
-'''
-
-# %%
